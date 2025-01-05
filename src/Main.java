072393bbfc418966992d3d07efc443089d03552e
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 23:50:15 +0530

    uc1 commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+
+        // UC1: Adding a contact
+        Contact contact1 = new Contact("John", "Doe", "123 Main St", "Springfield", "Illinois", "62704", "1234567890", "john.doe@example.com");
+        addressBook.addContact(contact1);
+    }
+}
